07942e42007279d2e1795f4d6e6c83725014aab0
PrajwalChaudhary25 <prajwalchy25@gmail.com>
2025-09-30 01:20:07 +0545

    configured react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/HomeSection'
+import Result from './components/ResultSection'
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Home/>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/result" element={<Result/>}></Route>
+          </Routes>
     </div>
-    
-
   );
 }
 
---
+++ frontend/src/components/ResultSection.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ResultSection extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                <h2>This is the result section</h2>
+                <p>Here we are going to see output of grading</p>
+            </div>
+        );
+    }
+}
+ 
+export default ResultSection
+;
