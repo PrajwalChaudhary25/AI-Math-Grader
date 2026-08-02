ae2760db1744372641fe81c8d5fefd002055d413
PrajwalChaudhary25 <prajwalchy25@gmail.com>
2026-08-02 13:46:21 +0545

    added project showcase to readme

--- frontend/src/components/Drag&DropUpload.js
+++ frontend/src/components/Drag&DropUpload.js
@@ -1,16 +1,30 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
+import { X } from "lucide-react";
 
 export default function SingleImageUpload({ onFileSelect, headerText, initialFile = null }) {
   const [image, setImage] = useState(initialFile);
   const [dragOver, setDragOver] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const inputRef = useRef(null);
+  
 
   useEffect(() => {
     setImage(initialFile);
   }, [initialFile]);
 
+   useEffect(() => {
+    if (isModalOpen) {
+      document.body.classList.add('overflow-hidden');
+    } else {
+      document.body.classList.remove('overflow-hidden');
+    }
+
+    // Cleanup when component unmounts
+    return () => document.body.classList.remove('overflow-hidden');
+  }, [isModalOpen]);
+
   // This function validates and sets images file
-  const handleFile = (file) => {
+  const handleFile = useCallback((file) => {
     if (!file) return;
     if (!file.type || !file.type.startsWith("image/")) {
       alert("Please upload an image only.");
@@ -18,7 +32,7 @@ export default function SingleImageUpload({ onFileSelect, headerText, initialFil
     }
     setImage(file);
     if (onFileSelect) onFileSelect(file);
-  };
+  }, [onFileSelect]);
 
   // This function handles events when files are dropped in the drop zone
   const onDrop = (e) => {
@@ -75,11 +89,12 @@ export default function SingleImageUpload({ onFileSelect, headerText, initialFil
       {/* Preview */}
       {image && (
         <div className="mt-4 flex items-center gap-4">
-          <img
-            src={typeof image === 'string' ? image : URL.createObjectURL(image)}
-            alt="preview"
-            className="w-24 h-24 rounded-lg object-cover shadow"
-          />
+            <img
+              src={typeof image === 'string' ? image : URL.createObjectURL(image)}
+              alt="preview"
+              className="w-36 h-36 rounded-lg object-cover shadow-black shadow-xs hover:scale-105 transition-transform duration-300 hover:cursor-pointer hover:shadow-2xl"
+              onClick={() => setIsModalOpen(true)}
+            />
 
           <button
             className="text-red-600 underline"
@@ -89,6 +104,30 @@ export default function SingleImageUpload({ onFileSelect, headerText, initialFil
           </button>
         </div>
       )}
+
+      {isModalOpen && (
+        <div 
+          className="fixed inset-0 bg-black bg-opacity-60 backdrop-blur-xl flex justify-center items-center z-50"
+          onClick={() => setIsModalOpen(false)}
+        >
+          <button
+            className="absolute top-4 right-10 text-red-500 hover:text-red-700 bg-gray-50 w-8 h-8 *: rounded-full flex justify-center items-center shadow hover:scale-110 transition-transform duration-300"
+            onClick={() => setIsModalOpen(false)}
+          >
+            <X size={24} />
+          </button>
+          <div 
+            className="bg-white p-6 rounded-lg max-w-lg max-h-[80vh] w-full  overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 select-none"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <img
+              src={typeof image === 'string' ? image : URL.createObjectURL(image)}
+              alt="preview"
+              className="w-full rounded-lg object-cover shadow"
+            />   
+          </div> 
+        </div>)
+            }
 
     </div>
   );
